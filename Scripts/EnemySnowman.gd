eeb5177237b7386fb03986f2fab96f40afdc08ee
Lucas <lucaslassiter@protonmail.com>
2024-12-01 23:38:58 -0800

    Flips snowman when turning back

--- Scripts/EnemySnowman.gd
+++ Scripts/EnemySnowman.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var sprite_2D = $"Sprite2D"
+
 
 const SPEED = 25.0
 const JUMP_VELOCITY = -400.0
@@ -24,3 +26,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Turn_Back_Node"):
 		moving_Right = moving_Right * -1
+		sprite_2D.flip_h = !sprite_2D.flip_h
+		
