d778e9059f2bb33a447a67487f70510d33bc77a9
Lucas <lucaslassiter@protonmail.com>
2024-12-01 23:33:29 -0800

    Added snowman turnback nodes

--- Scripts/EnemySnowman.gd
+++ Scripts/EnemySnowman.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 25.0
 const JUMP_VELOCITY = -400.0
+var moving_Right = -1
 
 
 func _physics_process(delta: float) -> void:
@@ -10,9 +11,16 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-
-	velocity.x = -1 * SPEED
+	velocity.x = moving_Right * SPEED
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass
+	
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Turn_Back_Node"):
+		moving_Right = moving_Right * -1
